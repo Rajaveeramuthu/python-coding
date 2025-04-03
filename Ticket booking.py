e66c1f9e33780dea2e49db85e8b997936a98406d
_RVM_ <rajaveeramuthu2509@gmail.com>
2025-04-03 10:42:58 +0530

    Add files via upload
    ticket booking progam

---
+++ Ticket booking.py
@@ -0,0 +1,9 @@
+ticket=int(input("How many ticket you want ?"))
+
+if ticket>10:
+    print("Ticket soled out")
+elif ticket==10:
+    print("you can book the ticket")
+else :
+    print("bokking successfuly")
+
